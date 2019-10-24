cfce8d8f6b357553f44130b47f3d70154a2b8179
Kioto Hirahara <kioto.hirahara@gmail.com>
2019-10-24 09:30:25 +0100

    Add switch input sample

---
+++ 01_switch.py
@@ -0,0 +1,23 @@
+#
+# 01_switch.py
+#
+
+import RPi.GPIO as GPIO
+import time
+
+SWITCH_PORT = 18
+
+GPIO.setmode(GPIO.BCM)          # GPIO番号で指定
+GPIO.setup(SWITCH_PORT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+state = False
+while True:
+    current_state = GPIO.input(SWITCH_PORT)
+    if current_state != state:
+        if current_state:
+            print('OFF')
+        else:
+            print('Pushed')
+
+        state = current_state
+
---
+++ 01_switch_inter.py
@@ -0,0 +1,23 @@
+#
+# 01_switch_inter.py
+#
+
+import RPi.GPIO as GPIO
+import time
+
+SWITCH_PORT = 18
+
+GPIO.setmode(GPIO.BCM)          # GPIO番号で指定
+GPIO.setup(SWITCH_PORT, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def cb_switch(ch):
+    current_state = GPIO.input(SWITCH_PORT)
+    if current_state:
+        print('OFF')
+    else:
+        print('Pushed')
+
+
+GPIO.add_event_detect(SWITCH_PORT, GPIO.BOTH, callback=cb_switch)
+
+time.sleep(10)
