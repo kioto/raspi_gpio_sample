aa6f454a4180739d0a01d66dd7e49c0cf3fe3fc1
Kioto Hirahara <kioto.hirahara@gmail.com>
2019-11-12 00:20:22 +0000

    Add Led lights with pigpio

---
+++ 02_led_pigpio_software_pwm_test.py
@@ -0,0 +1,36 @@
+#
+# 02_led_pigpio_sofware_pwm_test.py
+#
+
+import sys
+import time
+import pigpio
+
+LED_PORT = 18
+RANGE = 100
+
+def main(freq, duty):
+    # Software PWM
+    pi = pigpio.pi()
+    pi.set_mode(LED_PORT, pigpio.OUTPUT)
+    pi.set_PWM_frequency(LED_PORT, freq)
+    pi.set_PWM_range(LED_PORT, RANGE)
+
+    try:
+        pi.set_PWM_dutycycle(LED_PORT, duty)
+        time.sleep(60)
+
+    except KeyboardInterrupt:
+        pass
+
+    # ピンをINPUTモードにしておかないと、LEDが点灯し続けてしまう
+    pi.set_mode(LED_PORT, pigpio.INPUT)
+    pi.stop()
+
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Usage: python %s <frequency> <duty_cycle>' % sys.argv[0])
+        exit()
+
+    main(int(sys.argv[1]), float(sys.argv[2]))
